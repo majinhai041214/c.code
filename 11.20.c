1313932978fdb9b95036d56bf0dbe5a7071241e2
majinhai041214 <151809529+majinhai041214@users.noreply.github.com>
2023-11-25 18:22:09 +0800

    Add files via upload

---
+++ 11.20.c
@@ -0,0 +1,20 @@
+// #include<stdio.h>
+// #include"11.20.h"
+// // int main()
+// {
+//     int a=6;
+//     int b=5;
+//     printf("%d",max(a,b));
+//     return 0;
+// }
+#include<stdio.h>
+int main()
+{
+    // int a=6;
+    // int t;
+
+    // t=printf("%i\n",a);
+    // printf("%d",t);
+    // return 0;
+    FILE*fp=fopen()
+}
